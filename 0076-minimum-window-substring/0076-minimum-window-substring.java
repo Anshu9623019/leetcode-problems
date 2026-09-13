2b8e5693cc120c311bcc066007ae82c2a369371f
Anshu kumar <91007770+Anshu9623019@users.noreply.github.com>
2026-09-14 01:26:12 +0530

    Time: 20 ms (37.57%), Space: 48.1 MB (6.85%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,112 +1,113 @@
 import java.util.*;
 
 class Solution {
-    // public String minWindow(String s, String t) {
-    //     if(s.length()<t.length()) return "";
-    //     if (t.length() == 0) return "";
-
-
-    //     HashMap<Character,Integer> need = new HashMap<>();
-
-    //     for(char c : t.toCharArray()){
-    //         need.put(c,need.getOrDefault(c,0)+1);
-    //     }
-
-    //     int left = 0; int right = 0;
-    //     int n = s.length();
-    //     int have = 0;
-    //     int minLen = Integer.MAX_VALUE;
-    //     int startInd = -1;
-    //     int needCnt = need.size();
-    //     HashMap<Character,Integer> window = new HashMap<>();
-
-    //     while(right<n){
-    //         char c = s.charAt(right);
-    //         window.put(c,window.getOrDefault(c,0)+1);
-
-    //         if(need.containsKey(c) && window.get(c)==need.get(c)){
-    //             have++;
-    //         }
-
-    //         while(have==needCnt){
-    //             int temp = (right - left + 1);
-    //             if(temp<minLen){
-    //                 minLen = temp;
-    //                 startInd = left;
-    //             }
-                
-    //             //Shrink
-    //             char cl = s.charAt(left);
-    //             window.put(cl,window.get(cl)-1);
-    //             if(need.containsKey(cl) && window.get(cl)<need.get(cl)){
-    //                 have--;
-    //             }
-    //             left++;
-    //         }
-    //         right++;
-    //     }
-    //     return minLen == Integer.MAX_VALUE ? "" : s.substring(startInd, startInd + minLen);
-    // }
-
     public String minWindow(String s, String t) {
+        if(s.length()<t.length()) return "";
+        if (t.length() == 0) return "";
 
-    if (t.length() == 0) return "";
-    if (s.length() < t.length()) return "";
 
-    HashMap<Character, Integer> need = new HashMap<>();
+        HashMap<Character,Integer> need = new HashMap<>();
 
-    for (char c : t.toCharArray()) {
-        need.put(c, need.getOrDefault(c, 0) + 1);
-    }
-
-    int left = 0;
-    int right = 0;
-
-    int have = 0;
-    int needCnt = need.size();
-
-    int minLen = Integer.MAX_VALUE;
-    int startInd = -1;
-
-    HashMap<Character, Integer> window = new HashMap<>();
-
-    while (right < s.length()) {
-
-        char c = s.charAt(right);
-
-        window.put(c, window.getOrDefault(c, 0) + 1);
-
-        if (need.containsKey(c)
-                && window.get(c).equals(need.get(c))) {
-            have++;
+        for(char c : t.toCharArray()){
+            need.put(c,need.getOrDefault(c,0)+1);
         }
 
-        while (have == needCnt) {
+        int left = 0; int right = 0;
+        int n = s.length();
+        int have = 0;
+        int minLen = Integer.MAX_VALUE;
+        int startInd = -1;
+        int needCnt = need.size();
+        HashMap<Character,Integer> window = new HashMap<>();
 
-            int len = right - left + 1;
+        while(right<n){
+            char c = s.charAt(right);
+            window.put(c,window.getOrDefault(c,0)+1);
 
-            if (len < minLen) {
-                minLen = len;
-                startInd = left;
+            if(need.containsKey(c) && window.get(c).equals(need.get(c))){
+                have++;
             }
+            
 
-            char cl = s.charAt(left);
-
-            window.put(cl, window.get(cl) - 1);
-
-            if (need.containsKey(cl)
-                    && window.get(cl) < need.get(cl)) {
-                have--;
+            while(have==needCnt){
+                int temp = (right - left + 1);
+                if(temp<minLen){
+                    minLen = temp;
+                    startInd = left;
+                }
+                
+                //Shrink
+                char cl = s.charAt(left);
+                window.put(cl,window.get(cl)-1);
+                if(need.containsKey(cl) && window.get(cl)<need.get(cl)){
+                    have--;
+                }
+                left++;
             }
-
-            left++;
+            right++;
         }
-
-        right++;
+        return minLen == Integer.MAX_VALUE ? "" : s.substring(startInd, startInd + minLen);
     }
 
-    return minLen == Integer.MAX_VALUE
-            ? ""
-            : s.substring(startInd, startInd + minLen);
-}
+//     public String minWindow(String s, String t) {
+
+//     if (t.length() == 0) return "";
+//     if (s.length() < t.length()) return "";
+
+//     HashMap<Character, Integer> need = new HashMap<>();
+
+//     for (char c : t.toCharArray()) {
+//         need.put(c, need.getOrDefault(c, 0) + 1);
+//     }
+
+//     int left = 0;
+//     int right = 0;
+
+//     int have = 0;
+//     int needCnt = need.size();
+
+//     int minLen = Integer.MAX_VALUE;
+//     int startInd = -1;
+
+//     HashMap<Character, Integer> window = new HashMap<>();
+
+//     while (right < s.length()) {
+
+//         char c = s.charAt(right);
+
+//         window.put(c, window.getOrDefault(c, 0) + 1);
+
+//         if (need.containsKey(c)
+//                 && window.get(c).equals(need.get(c))) {
+//             have++;
+//         }
+
+//         while (have == needCnt) {
+
+//             int len = right - left + 1;
+
+//             if (len < minLen) {
+//                 minLen = len;
+//                 startInd = left;
+//             }
+
+//             char cl = s.charAt(left);
+
+//             window.put(cl, window.get(cl) - 1);
+
+//             if (need.containsKey(cl)
+//                     && window.get(cl) < need.get(cl)) {
+//                 have--;
+//             }
+
+//             left++;
+//         }
+
+//         right++;
+//     }
+
+//     return minLen == Integer.MAX_VALUE
+//             ? ""
+//             : s.substring(startInd, startInd + minLen);
+// }
 }
